1d397eb59e2a5f419dc233d22d1e1edabb4e57bc
sumith006 <kolekarsumith@gmail.com>
2024-11-23 11:23:43 +0530

    js

---
+++ src/Jedi.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+class Jedi extends React.Component {
+    render() {
+        return (
+            <div>I am a Jedi Component</div>
+        );
+    }
+}
+export default Jedi;  
